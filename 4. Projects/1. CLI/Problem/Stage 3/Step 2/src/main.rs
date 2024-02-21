d7c396c3488b0097aaa98c382d28da3e0eb01089
Eduardo <eduardofernandezvillazon4@gmail.com>
2024-02-21 08:38:39 +0000

    Actions in main function completed, program printing main page but unable to navigate

--- 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
+++ 4. Projects/1. CLI/Problem/Stage 3/Step 2/src/main.rs
@@ -6,6 +6,7 @@ mod db;
 use db::*;
 
 mod ui;
+use ui::*;
 
 mod io_utils;
 use io_utils::*;
@@ -13,12 +14,35 @@ use io_utils::*;
 mod navigator;
 use navigator::*;
 
+
+
 fn main() {
+    let database = Rc::new(JiraDatabase::new("./data/db.json".to_string()));
+    let mut navigator = Navigator::new(database);
     // TODO: create database and navigator
     
     loop {
         clearscreen::clear().unwrap();
-
+        let page : Option<&Box<dyn Page>> = navigator.get_current_page();
+        match page{
+            Some(page) => {
+                page.draw_page().unwrap();
+                let input = get_user_input();
+                println!("{}", input);
+                let action = page.handle_input(&input);
+                match action {
+                    Ok(Some(action)) => {
+                        navigator.handle_action(action).unwrap();
+                    },
+                    Ok(None) => {},
+                    Err(e) => {
+                        println!("Error: {}", e);
+                    }
+                }
+            },
+            None => break
+        }
+        
         // TODO: implement the following functionality:
         // 1. get current page from navigator. If there is no current page exit the loop.
         // 2. render page
